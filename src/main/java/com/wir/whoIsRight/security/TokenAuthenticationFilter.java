6efa088c2372d6623fdd6a08ea5cd059e33562d8
maricic89 <maricic89@gmail.com>
2019-05-05 21:30:02 +0200

    Login with facebook fix

--- src/main/java/com/wir/whoIsRight/security/TokenAuthenticationFilter.java
+++ src/main/java/com/wir/whoIsRight/security/TokenAuthenticationFilter.java
@@ -1,9 +1,8 @@
 package com.wir.whoIsRight.security;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,18 +10,18 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-
-@AllArgsConstructor
-@NoArgsConstructor
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
 
+    @Autowired
     private TokenProvider tokenProvider;
+    @Autowired
     private CustomUserDetailsService customUserDetailsService;
 
     private static final Logger logger = LoggerFactory.getLogger(TokenAuthenticationFilter.class);
